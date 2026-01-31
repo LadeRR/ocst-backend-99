21c2102b06161aa4c2a621b4a4952d7039310b2c
LadeRR <89153889+LadeRR@users.noreply.github.com>
2026-01-31 07:12:56 +0300

    Update server.js
    webhook

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ let chatMessages = [];
 
 // Discord Webhook URLs (Bunları kendi webhook URL'lerinizle değiştirin)
 const DISCORD_WEBHOOKS = {
-  chat: 'YOUR_CHAT_WEBHOOK_URL', // Chat log kanalı
-  pager: 'YOUR_PAGER_WEBHOOK_URL' // Pager kanalı
+  chat: 'https://discord.com/api/webhooks/1466676305050472615/vZTS9bnlMeCYTABz5p0Cnwlv0yvP_qcDdhspbdNowdrSJmCifSaT1BJ7lLmWTH_ob-72', // Chat log kanalı
+  pager: 'https://discord.com/api/webhooks/1466651197363454072/LbukP7UrHVqusJLzx7f7s1PMatzpB2L20h5LNT41NeUtLCRe9OMNc9rPlhh9_rrO_34S' // Pager kanalı
 };
 
 // Discord'a mesaj gönderme fonksiyonu
@@ -207,4 +207,5 @@ const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
   console.log(`OCST Dispatch Backend çalışıyor: ${PORT}`);
+
 });
